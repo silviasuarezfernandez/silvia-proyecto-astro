f616b6dc5ffaf2cb8d18579215e1121cdfc139be
silviasuarezfernandez <silviasuarez50@gmail.com>
2025-05-04 12:48:17 +0200

    archivos nuevos

---
+++ src/components/Contador.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+
+export default function Contador() {
+  const [contador, setContador] = useState(0);
+
+  return (
+    <div>
+      <p>Contador: {contador}</p>
+      <button onClick={() => setContador(contador + 1)}>Incrementar</button>
+    </div>
+  );
+}
+
